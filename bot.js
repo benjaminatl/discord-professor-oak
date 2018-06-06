e4f454e2c162b4f99ce82be61438edd47107f1c7
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-06 15:34:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 ​
 // Set the prefix
-let prefix = "?oak";
+let prefix = "?";
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
