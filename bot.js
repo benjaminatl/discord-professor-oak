710877325673b5f8e03d6a41b0c7b4d6c3bd605a
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 14:59:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 const config = require("./config.json");
-
 
 client.on("message", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
