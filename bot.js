54b0d04d15dab29bf62e8149381257f70da651e0
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 14:09:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,34 @@
-const discord = require('discord.js');
-const handles = require('discord-handles');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+
 const config = require("./config.json");
 
-const client = new discord.Client();
-const handler = new handles.Client(client);
 
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require("./events/" + `${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
 
+client.on("message", message => {
+  if (message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  // This is the best way to define args. Trust me.
+  const args = message.content.split(/\s+/g);
+  const command = args.shift().slice(config.prefix.length).toLowerCase();
+
+  // The list of if/else is replaced with those simple 2 lines:
+  try {
+    let commandFile = require("./commands/" + `${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
