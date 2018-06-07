83056ae30ee1fc24280ea6f0707ff60a31f0a3a2
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 16:23:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,18 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  // This is where we'll put our code.
-  if (message.content.indexOf(config.prefix) !== 0) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  if(command === 'ping') {
-    message.channel.send('Pong!');
-  } else
-  if (command === 'blah') {
-    message.channel.send('Meh.');
-  } else
-  if (command === 'test') {
-    message.channel.send({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
-}})};
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  }
+  
+  else
+    
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
