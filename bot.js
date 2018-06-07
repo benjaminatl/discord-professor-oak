3f60224f8872628b3f599014a0ef073fa425e4eb
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 14:00:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,10 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const discord = require('discord.js');
+const handles = require('discord-handles');
 const config = require("./config.json");
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+const client = new discord.Client();
+const handler = new handles.Client(client);
 
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(config.prefix + "foo")) {
-    message.channel.send("bar!");
-  }
-});
+
 
 client.login(process.env.BOT_TOKEN);
