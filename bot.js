a0b2c37cdbea1062fc787199aff819bdea42e2f2
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 15:27:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,21 @@
 const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
-
 const client = new Discord.Client();
 const config = require("./config.json");
-client.config = config;
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
-
-client.commands = new Enmap();
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
+client.on("message", message => {
+  if (message.author.bot) return;
+  // This is where we'll put our code.
+  if (message.content.indexOf(config.prefix) !== 0) return;
+​
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+​
+  if(command === 'ping') {
+    message.channel.send('Pong!');
+  } else
+  if (command === 'blah') {
+    message.channel.send('Meh.');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
