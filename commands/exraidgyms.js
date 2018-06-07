9a0b9a68d51f5750e8a099f8b72f57fc17552c83
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 09:50:54 -0400

    Update and rename exraidgyms.js to hello.js

--- commands/exraidgyms.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-bot.on('message', (message) => {
-    if(message.content == 'exraidgyms') {
-        message.channel.sendMessage('Trainer, you could receive an Ex Raid pass from raiding at **East Carroll** and **Starbucks** in Carrollton.');
-    }
-});
