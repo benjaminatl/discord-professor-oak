bb7411dd9d1c0c7093f51bef2bf507c48e9ff127
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 09:44:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-​
+
 // Set the prefix
 let prefix = "!";
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-​
+
   if (message.content.startsWith(prefix + "hi")) {
     message.channel.send("Hello, Trainer!");
   } else
@@ -14,6 +14,5 @@ client.on("message", (message) => {
     message.channel.send("Hello Trainer, what can I help you with?");
   }
 });
-
 
 client.login(process.env.BOT_TOKEN);
