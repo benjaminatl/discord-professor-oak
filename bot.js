3c6cf1fe4dfa318484dd5a791fb7f7c638d09013
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 14:56:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
 
 const config = require("./config.json");
 
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    let eventFunction = require("./events/" + `${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, (...args) => eventFunction.run(client, ...args));
-  });
-});
-
-client.on("message", message => {
-  if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-
-  // This is the best way to define args. Trust me.
-  const args = message.content.split(/\s+/g);
-  const command = args.shift().slice(config.prefix.length).toLowerCase();
-
-  // The list of if/else is replaced with those simple 2 lines:
-  try {
-    let commandFile = require("./commands/" + `${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+​
+  if (message.content.startsWith(config.prefix + "Hi")) {
+    message.channel.send("Hello, Trainer!");
+  } else
+  if (message.content.startsWith(config.prefix + "Help")) {
+    message.channel.send("Hello Trainer, what did you need help with?");
   }
 });
 
