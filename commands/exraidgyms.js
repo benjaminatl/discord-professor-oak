bb2a8666f15833768cf8925f8cce265f222ff1de
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 09:11:45 -0400

    Update exraidgyms.js

--- commands/exraidgyms.js
+++ commands/exraidgyms.js
@@ -1,1 +1,8 @@
-ex raid gyms
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+    if(message.content == 'exraidgyms') {
+        message.channel.sendMessage('Trainer, you could receive an Ex Raid pass from raiding at **East Carroll** and **Starbucks** in Carrollton.');
+    }
+});
