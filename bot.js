fa31b056898651d3248b46c0ea4987239fefbf69
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 10:55:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,20 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
+const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
 
 client.commands = new Enmap();
 
@@ -18,5 +28,4 @@ fs.readdir("./commands/", (err, files) => {
     client.commands.set(commandName, props);
   });
 });
-
 client.login(process.env.BOT_TOKEN);
