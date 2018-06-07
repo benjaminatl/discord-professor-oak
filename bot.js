309b1744edb98482d5f5ddefc9f2a59e1fe486a5
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-07 09:29:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,15 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require("./config.json");
 
-bot.on('message', (message) => {
-    if(message.content == (config.prefix + 'Hey')) {
-        message.channel.sendMessage('Hello, Trainer');
-    }
+bot.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+​
+  if (message.content.startsWith(config.prefix + "Hey")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "Hey, Trainer!")) {
+    message.channel.send("bar!");
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
