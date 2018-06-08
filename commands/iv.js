064106e641b7e5f8b339cab4d3de50adcbb5b9cf
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 15:48:41 -0400

    Update iv.js

--- commands/iv.js
+++ commands/iv.js
@@ -5,8 +5,8 @@ exports.run = (client, message, args) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "What is Pokemon IV?"
-    description: "Hello Trainer! I see that you are interested in knowing more about Pokemon IV's... IV stands for Individual Values. IVs are a mechanism to introduce diversity in a group of same species Pokemon. IV influences various other Pokemon stats, like the amount of Health Points, attack power, etc….",
+    title: "What is Pokemon IV?",
+    description: "Hello Trainer! I see that you are interested in knowing more about Pokemon IV's... IV stands for Individual Values. IVs are a mechanism to introduce diversity in a group of same species Pokemon. IV influences various other Pokemon stats, like the amount of Health Points, attack power, etc",
     fields: [{
         name: "Check IV on IOS Device",
         value: "Download **Poke Genie** from the app store"
