2acafc74629d9fa1df0a036a97dca2fa100d60e3
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 15:11:25 -0400

    Update eastcarroll.js

--- commands/eastcarroll.js
+++ commands/eastcarroll.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send("<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.220053691904!2d-85.04636868446408!3d33.599594448894116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDM1JzU4LjUiTiA4NcKwMDInMzkuMSJX!5e0!3m2!1sen!2sus!4v1528484911433" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>").catch(console.error);
+    message.channel.send("Hello, Trainer. I see you need directions to **East Carroll**. I have found that information for you, see below.").catch(console.error);,
+    message.channel.send("https://goo.gl/maps/ZSCWZnXeKt32").catch(console.error);
 }
