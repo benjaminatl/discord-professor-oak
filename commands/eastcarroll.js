60241d7c83ebd627d2b6b0e4656dfa867cfcd21f
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 15:08:59 -0400

    Update eastcarroll.js

--- commands/eastcarroll.js
+++ commands/eastcarroll.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("https://www.google.com/maps/d/u/0/edit?mid=1rgYwn9FzHC1G1Ir4SCGbO4I_JeJLkHKb&ll=33.599589778059766%2C-85.04443354240254&z=19").catch(console.error);
+    message.channel.send("<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.220053691904!2d-85.04636868446408!3d33.599594448894116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDM1JzU4LjUiTiA4NcKwMDInMzkuMSJX!5e0!3m2!1sen!2sus!4v1528484911433" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>").catch(console.error);
 }
