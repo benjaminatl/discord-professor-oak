9fd68a9663eba0b68f8907bb16d40c8b059d889d
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 11:52:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,8 @@ client.on("message", (message) => {
   
 // START OF ALL COMMANDS
   
-  // HELP
+      
+        // HELP
   if (message.content.startsWith(prefix + "help")) {
     message.channel.send({embed: {
     color: 3447003,
@@ -38,39 +39,8 @@ client.on("message", (message) => {
       icon_url: client.user.avatarURL,
       text: "© Example"
     }
-  } else
-      
-        // HELP
-  if (message.content.startsWith(prefix + "one")) {
-    message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
   }
-});else
+});
       
   
   // CLOSE OF CLIENT.ON  
