04578e80c502bbd49fc7efaacf3291720ead18fe
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 15:12:48 -0400

    Update eastcarroll.js

--- commands/eastcarroll.js
+++ commands/eastcarroll.js
@@ -1,4 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Hello, Trainer. I see you need directions to **East Carroll**. I have found that information for you, see below.").catch(console.error);,
-    message.channel.send("https://goo.gl/maps/ZSCWZnXeKt32").catch(console.error);
+    message.channel.send("Hello, Trainer. I see you need directions to the gym **East Carroll**. I have found that information for you, https://goo.gl/maps/ZSCWZnXeKt32").catch(console.error);
 }
