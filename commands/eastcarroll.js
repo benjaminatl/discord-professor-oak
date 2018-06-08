3a50fa0fad76011a9061f494e67fb5d6bddcf307
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 15:05:19 -0400

    Update eastcarroll.js

--- commands/eastcarroll.js
+++ commands/eastcarroll.js
@@ -1,9 +1,3 @@
 exports.run = (client, message, args) => {
-  message.channel.send({embed: {
-  color: 3447003,
-  title: "East Carroll",
-  description: "https://www.google.com/maps/d/u/0/edit?mid=1rgYwn9FzHC1G1Ir4SCGbO4I_JeJLkHKb&ll=33.59935478241004%2C-85.0441384994109&z=19"
-}});
+    message.channel.send("https://www.google.com/maps/d/u/0/edit?mid=1rgYwn9FzHC1G1Ir4SCGbO4I_JeJLkHKb&ll=33.599589778059766%2C-85.04443354240254&z=19").catch(console.error);
 }
-
-
