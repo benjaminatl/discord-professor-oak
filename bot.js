b62c863ffe7205090058b1749804b2396801151a
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 14:04:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,21 +4,32 @@ const config = require("./config.json");
 const prefix = config.prefix
 const fs = require("fs");
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-  
-  
-// START OF ALL COMMANDS
-      
+// This loop reads the /events/ folder and attaches each event file to the appropriate event.
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  // This is the best way to define args. Trust me.
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // The list of if/else is replaced with those simple 2 lines:
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
   } catch (err) {
     console.error(err);
   }
-  
-  
-  // CLOSE OF CLIENT.ON  
 });
 
   
