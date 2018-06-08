2fa28ef3f4e2d8a6047bba964af4462a78797ab9
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 15:59:16 -0400

    Update iv.js

--- commands/iv.js
+++ commands/iv.js
@@ -9,11 +9,11 @@ exports.run = (client, message, args) => {
     description: "Hello Trainer! I see that you are interested in knowing more about Pokemon IV's... IV stands for Individual Values. IVs are a mechanism to introduce diversity in a group of same species Pokemon. IV influences various other Pokemon stats, like the amount of Health Points, attack power, etc",
     fields: [{
         name: "Check IV on IOS Device",
-        value: "Download **Poke Genie** from the app store"
+        value: "Download *__Poke Genie__* from the app store"
       },
       {
         name: "Check IV on Android Device",
-        value: "Download **Calcy IV** from the Google Play store."
+        value: "Download *__Calcy IV__* from the Google Play store."
       },
       {
         name: "More Information",
