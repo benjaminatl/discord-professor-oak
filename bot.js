f1e73e464ed76c098e9aa3cc73d39a6dd2fadc20
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 09:59:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,13 +5,20 @@ const config = require("./config.json");
 client.on("message", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(config.prefix + "foo")) {
-    message.channel.send({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
-});
+
+  
+  const user = message.mentions.users.first();
+  const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+  if (!amount) return message.reply('Must specify an amount to delete!');
+  if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+  message.channel.fetchMessages({
+  limit: amount,
+  }).then((messages) => {
+  if (user) {
+  const filterBy = user ? user.id : Client.user.id;
+  messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+  }
+  message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+  });
   
 client.login(process.env.BOT_TOKEN);
