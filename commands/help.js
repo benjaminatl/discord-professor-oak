ffd5dfd9b4a106555087bf79d1f5a6a3dc64d38c
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 14:15:21 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const prefix = config.prefix
+const fs = require("fs");
+
 client.on("message", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
     message.channel.send({embed: {
