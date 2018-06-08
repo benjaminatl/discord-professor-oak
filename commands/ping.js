7fceaa306c31abdf3eb29b7a30e709ccf7f13fdd
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 09:08:28 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,14 @@
-exports.run = async (bot, message, args) => { // This function takes three arguments, the bot (client) message (full message with prefix etc.) and args (Arguments of command)
-  message.reply(`Pong! ${bot.ping}`); // Sends message with bot latency and short message (Pong!)
-} 
+// We created it under the /commands/ folder since we specified that in the server.js file.
 
-exports.help = {
-  name: 'ping' // Insert your command's name here!
-}
+exports.run = (client, message, args, tools) => { // Since we passed in these arguments in the previous file, we need to then fetch them here. - Since we passed it in server.js, we also need to recieve it in the command files. 
+
+  // We won't need that, since tools.embed() sends a message. 
+  
+  tools.embed(message.channel, '**Ping!**', 10000); // We are passing it the channel to send it to, as well as the message (discord formatting supported), as well as the the time in ms to delete the message.
+  
+  // Let's test it!
+  
+  // message.channel.send(tools.ping()); // This will type 'Pong!' in chat, since we are running the entire file.
+  // Now, to call it we have to call the function name. Make sure you are calling it like a function with () at the end.
+  
+};
