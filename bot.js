39314410fd8743e716b25fd50489f9c383329b56
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 11:13:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,26 +17,18 @@ client.on("message", (message) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
+    title: "Help Commands",
+    description: "Hello Trainer, Below is a list of commands I have learned through my research.",
     fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
+        name: "help",
+        value: "Displays this message."
       },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
+      
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© Example"
+      text: "© Professor Oak, Carrollton GA"
     }
   }
 });
