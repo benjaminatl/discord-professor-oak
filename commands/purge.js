040a5fe7fac563657e10c1f507f95bdc456f57e3
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 14:50:46 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,13 +1,40 @@
-const user = message.mentions.users.first();
-const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-if (!amount) return message.reply('Must specify an amount to delete!');
-if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-message.channel.fetchMessages({
- limit: amount,
-}).then((messages) => {
- if (user) {
- const filterBy = user ? user.id : Client.user.id;
- messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
- }
- message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-});
+exports.commands = [
+  'purge' // command that is in this file, every command needs it own export as shown below
+];
+
+exports.purge = {
+  usage: '<number of messages>',
+  description: 'Deletes Messages',
+  process: function(bot, msg, suffix) {
+    if (inPrivate(msg)) {
+      msg.channel.send("You Cant Purge Message In DM's!");
+      return;
+    }
+    if (hasPerms(msg)) {
+      if (!suffix) {
+        var newamount = '2';
+      } else {
+        var amount = Number(suffix);
+        var adding = 1;
+        var newamount = amount + adding;
+      }
+      let messagecount = newamount.toString();
+      msg.channel
+        .fetchMessages({
+          limit: messagecount
+        })
+        .then(messages => {
+          msg.channel.bulkDelete(messages);
+          // Logging the number of messages deleted on both the channel and console.
+          msg.channel.send('Deletion of messages successful. \n Total messages deleted including command: ' + newamount).then(message => message.delete(5000));
+          console.log('Deletion of messages successful. \n Total messages deleted including command: ' + newamount);
+        })
+        .catch(err => {
+          console.log('Error while doing Bulk Delete');
+          console.log(err);
+        });
+    } else {
+      msg.channel.send('only moderators can use this command!').then(message => message.delete(5000));
+    }
+  }
+};
