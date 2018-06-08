4ad6933a5ee9531f7d77a1f8d3ef59c44fc1eab4
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 11:49:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,47 +17,62 @@ client.on("message", (message) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "Help Commands",
-    description: "Hello Trainer, Below is a list of commands I have learned through my research. To execute any command, type the below commands in bold exactly how they appear.",
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
     fields: [{
-        name: "oak help",
-        value: "Displays this message."
+        name: "Fields",
+        value: "They can have different fields with small headlines."
       },
-      
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© Professor Oak, Carrollton GA"
+      text: "© Example"
     }
   }
-});
-  } else
-  
-    // HELP
-  if (message.content.startsWith(prefix + "alolan")) {
+});else
+      
+        // HELP
+  if (message.content.startsWith(prefix + "one")) {
     message.channel.send({embed: {
     color: 3447003,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "Help Commands",
-    description: "Hello Trainer, Below is a list of commands I have learned through my research. To execute any command, type the below commands in bold exactly how they appear.",
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
     fields: [{
-        name: "oak help",
-        value: "Displays this message."
+        name: "Fields",
+        value: "They can have different fields with small headlines."
       },
-      
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© Professor Oak, Carrollton GA"
+      text: "© Example"
     }
   }
-});
-  } else
+});else
+      
   
   // CLOSE OF CLIENT.ON  
 });
