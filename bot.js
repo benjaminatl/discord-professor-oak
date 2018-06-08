48b037a05532a248ede11fcefcc1c51fd6013e30
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 10:19:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,6 @@ const config = require("./config.json");
 
 client.on("message", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-
-
-  
   
   
 // START OF ALL COMMANDS
@@ -23,25 +19,6 @@ client.on("message", (message) => {
   
   // CLOSE OF CLIENT.ON  
 });
-  
-  
-  
-  // PURGE MESSAGES
-  const user = message.mentions.users.first();
-  const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-    if (!amount) return message.reply('Must specify an amount to delete!');
-    if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-      message.channel.fetchMessages({
-      limit: amount,
-      }).then((messages) => {
-   if (user) {
-  const filterBy = user ? user.id : Client.user.id;
-    messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-    }
-    message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-    });
-  
-
 
   
 client.login(process.env.BOT_TOKEN);
