1e13bb3a4e7b96779983e6cc678bcf3ad1ec4685
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 16:23:37 -0400

    Create gymlist.js

---
+++ commands/gymlist.js
@@ -0,0 +1,118 @@
+exports.run = (client, message, args) => {
+    message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "What is Pokemon IV?",
+    description: "Hello Trainer! I see that you are interested in knowing more about Pokemon IV's... IV stands for Individual Values. IVs are a mechanism to introduce diversity in a group of same species Pokemon. IV influences various other Pokemon stats, like the amount of Health Points, attack power, etc",
+    fields: [{
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+      {
+        name: "Check IV on IOS Device",
+      },
+      {
+        name: "Check IV on Android Device",
+      },
+      {
+        name: "More Information",
+      },
+             
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Professor Oak"
+    }
+  }
+});
+}
