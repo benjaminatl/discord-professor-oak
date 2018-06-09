a127c377f4dcdef25f661b0bf59b44c6b775be52
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 22:54:02 -0400

    Update eeveetrick.js

--- commands/eeveetrick.js
+++ commands/eeveetrick.js
@@ -8,23 +8,23 @@ exports.run = (client, message, args) => {
     description: "Hello Trainer, Similarly to the anime and main series games, there is a special method to evolve Eevee. The trick is to give Eevee a specific nickname *(or rename)* before evolving it. This can be used only once per each evolution so it is highly recommended that you thoroughly plan before doing so. The trick with walking your buddy for Espeon and Umbreon has no limits.",
     fields: [{
         name: "Vaporeon",
-        value: "Remane to *Rainer*"
+        value: "Remane to *Rainer*, then evolve."
       },
       {
         name: "Jolteon",
-        value: "Remane to *Sparky*"
+        value: "Remane to *Sparky*, then evolve."
       },
       {
         name: "Flareon",
-        value: "Remane to *Pyro*"
+        value: "Remane to *Pyro*, then evolve."
       },
       {
         name: "Espeon",
-        value: "Remane to *Sakura*. You can also set Eevee as your buddy, earn 2 candy by walking 10KM and evolving during the day to receive this Pokemon."
+        value: "Remane to *Sakura*, then evolve. You can also set Eevee as your buddy, earn 2 candy by walking 10KM and evolving during the day to receive this Pokemon."
       },
       {
         name: "Umbreon",
-        value: "Remane to *Tamao*. You can also set Eevee as your buddy, earn 2 candy by walking 10KM and evolving during the night to receive this Pokemon."
+        value: "Remane to *Tamao*, then evolve. You can also set Eevee as your buddy, earn 2 candy by walking 10KM and evolving during the night to receive this Pokemon."
       },
     ],
     timestamp: new Date(),
