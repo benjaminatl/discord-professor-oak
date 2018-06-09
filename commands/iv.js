e6fd24c51ea93ae6a0a4f99eee3725477a024afd
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 23:12:37 -0400

    Update iv.js

--- commands/iv.js
+++ commands/iv.js
@@ -29,7 +29,7 @@ exports.run = (client, message, args) => {
     }
   }
 });
-}and
+},
     exports.run = (client, message, args) => {
     message.channel.send("Hello, Trainer. I see you need information on the gym **Starbucks**. My research shows: It is EX Raid eligable, access can be diffucult, must park in visitor parking on Front Campus, handicap parking is available closer to the gym, will require to get out of vechile and walk. https://goo.gl/maps/KKfSiunCdh22").catch(console.error);
 }
