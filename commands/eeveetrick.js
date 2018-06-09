e1d3c66e83faae922a38857be2d5680ff0bb2553
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 22:50:27 -0400

    Update eeveetrick.js

--- commands/eeveetrick.js
+++ commands/eeveetrick.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    description: "Hello Trainer, Similarly to the anime and main series games, there is a special method to evolve Eevee. The trick is to give Eevee a specific nickname *(or rename)* before evolving it. **This can be used only once per each evolution so it is highly recommended that you thoroughly plan before doing so**. ",
+    description: "Hello Trainer, Similarly to the anime and main series games, there is a special method to evolve Eevee. The trick is to give Eevee a specific nickname *(or rename)* before evolving it. This can be used only once per each evolution so it is highly recommended that you thoroughly plan before doing so. The trick with walking your buddy for Espeon and Umbreon has no limits.",
     fields: [{
         name: "Vaporeon",
         value: "Remane to *Rainer*"
