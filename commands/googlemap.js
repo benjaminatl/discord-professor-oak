05b7f39f4e94d00c815cb3cce560e48986d4abd4
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-09 19:28:05 -0400

    new command: googlemap

--- commands/googlemap.js
+++ commands/googlemap.js
@@ -1,1 +1,3 @@
-t
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. Long research has went into the Google Map. It now displays Gyms, Ex Raid eligable Gyms, Pokestops, in-game parks, and water spawns. https://drive.google.com/open?id=1rgYwn9FzHC1G1Ir4SCGbO4I_JeJLkHKb&usp=sharing").catch(console.error);
+}
