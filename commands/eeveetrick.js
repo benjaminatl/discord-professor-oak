09f293ff6358d31d5dd1367ca9d183dbd2ed61d0
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-08 22:17:56 -0400

    Update eeveetrick.js

--- commands/eeveetrick.js
+++ commands/eeveetrick.js
@@ -1,33 +1,37 @@
-const embed = new Discord.RichEmbed()
 exports.run = (client, message, args) => {
     message.channel.send({embed: {
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 1024 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-​
-  message.channel.send({embed});
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    description: "Hello Trainer, Similarly to the anime and main series games, there is a special method to evolve Eevee. The trick is to give Eevee a specific nickname before evolving it. **This can be used only once per each evolution so it is highly recommended that you thoroughly plan before doing so**. ",
+    fields: [{
+        name: "V",
+        value: "Will display this message *(list of commands)*."
+      },
+      {
+        name: "F",
+        value: "Get information on what Pokemon IV's are and how to check them."
+      },
+      {
+        name: "J",
+        value: "General information about a specific gym along with map directions."
+      },
+      {
+        name: "U",
+        value: "General information about a specific gym along with map directions."
+      },
+      {
+        name: "5",
+        value: "General information about a specific gym along with map directions."
+      },
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Professor Oak"
+    }
+  }
 });
 }
