21aab7e355f83b8f9fbc2a466a841da7518d8cc1
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-10 02:35:13 -0400

    update

--- pokemon/larvitar.js
+++ pokemon/larvitar.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 const embed = new Discord.RichEmbed()
   .setTitle("This is your title, it can hold 256 characters")
   .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
