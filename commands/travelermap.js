31364867c6c713d9c84229652e66637104fb36fc
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-09 21:23:13 -0400

    new command: travelermap

---
+++ commands/travelermap.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer! The **Traveler Map** is a great way for Trainers to connect with other Pokemon GO communities. See the map below to get connected. Also using the command `!oak nestatlas` to discover nests.").catch(console.error);
+}
