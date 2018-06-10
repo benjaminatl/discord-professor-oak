1d796c964e2c2de6717334034ddac3ad7a50daef
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-09 21:38:41 -0400

    updated response

--- commands/nestatlas.js
+++ commands/nestatlas.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Hello, Trainer! The **SilphRoad Nest Atlas** helps both local and traveling Trainers worldwide discover nests. The map is updated and verified by Trainers in their said community. If you plan on traveling, check the map below to find out which Pokemon are nesting at your destination. Also, using the command `!oak leaguemap` I can help you discover communites from around the world. https://thesilphroad.com/atlas#13.32/33.58055/-85.07418").catch(console.error);
+    message.channel.send("Hello, Trainer! The **SilphRoad Nest Atlas** helps both local and traveling Trainers worldwide discover nests. The map is updated and verified by Trainers in their said community. See the map below. Also, using the command `!oak leaguemap` I can help you discover communites from around the world. https://thesilphroad.com/atlas#13.32/33.58055/-85.07418").catch(console.error);
 }
