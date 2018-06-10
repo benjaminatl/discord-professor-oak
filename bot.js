07135a83faf4dd25594c7f96234f2dfc2ca89dad
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-10 01:56:28 -0400

    bug fix

--- bot.js
+++ bot.js
@@ -4,16 +4,7 @@ const config = require("./config.json");
 const prefix = config.prefix
 const fs = require("fs");
 
-// This loop reads the /events/ folder and attaches each event file to the appropriate event.
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    let eventFunction = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
-    client.on(eventName, (...args) => eventFunction.run(client, ...args));
-  });
-});
+
 
 client.on("message", message => {
   if (message.author.bot) return;
