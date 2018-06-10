ead967c5866cd02ab940feaf58086477e657ed64
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-09 23:58:06 -0400

    update test

--- pokemon/larvitar.js
+++ pokemon/larvitar.js
@@ -1,4 +1,3 @@
-const embed = new Discord.RichEmbed()
-    .setColor(0x4286F4)
-    .setDescription("The help has been sent to your DMs.")
-message.channel.send({embed});
+bot.on('message' message => {
+    message.channel.send("**LARVITAR**", {files: ["http://assets22.pokemon.com/assets/cms2/img/pokedex/full/246.png"]});
+});
