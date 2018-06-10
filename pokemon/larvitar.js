798d8e3c951501c362957993a2a437bbe3a6bb42
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-10 00:08:07 -0400

    update test

--- pokemon/larvitar.js
+++ pokemon/larvitar.js
@@ -1,3 +1,6 @@
 exports.run = (client, message, args) => {
-    message.channel.send("**LARVITAR**", {files: ["http://assets22.pokemon.com/assets/cms2/img/pokedex/full/246.png"]});
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Starbucks**. My research shows: It is EX Raid eligable, access can be diffucult, must park in visitor parking on Front Campus, handicap parking is available closer to the gym, will require to get out of vechile and walk. https://goo.gl/maps/KKfSiunCdh22").catch(console.error);
+}
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer! The **SilphRoad League Map** is a great way for Trainers to connect with other Pokemon GO communities. See the map below to get connected. Also using the command `!oak nestatlas` I can help you discover nests from around the world. https://thesilphroad.com/map#6.02/34.37/-83.771").catch(console.error);
 }
