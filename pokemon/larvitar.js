5d6a41f8ed716b152dfafc2418f556dd66e44bc8
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-10 00:03:03 -0400

    bug fix

--- pokemon/larvitar.js
+++ pokemon/larvitar.js
@@ -1,3 +1,3 @@
-bot.on('message' message => {
+exports.run = (client, message, args) => {
     message.channel.send("**LARVITAR**", {files: ["http://assets22.pokemon.com/assets/cms2/img/pokedex/full/246.png"]});
-});
+}
