77ae0da6eb2fb95e0436b0510d52430bbd667d97
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-10 04:00:47 -0400

    bug fix

--- pokemon/larvitar.js
+++ pokemon/larvitar.js
@@ -2,19 +2,20 @@ module.exports.run = (client, message, args) => {
   const Discord = require("discord.js");
 const embed = new Discord.RichEmbed()
 
-  .setTitle("Larvitar - #246")
-  .setAuthor("© Professor Oak", "client.user.avatarURL")
-
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  
   .setColor(0x00AE86)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
   .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setThumbnail("https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Larvitar-Pokemon-Go.png")
-
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  
   .setTimestamp()
   .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
   .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
-
+    "This is a field value, it can hold 1024 characters.")
+ 
   .addField("Inline Field", "They can also be inline.", true)
 
   .addBlankField(true)
