9cd0089993cbcfee1574d04fb1ee098900082ff0
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-11 21:47:35 -0400

    updated larvitar

--- pokemon/larvitar.js
+++ pokemon/larvitar.js
@@ -6,18 +6,20 @@ const embed = new Discord.RichEmbed()
   .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
   
   .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setDescription("Rock | Ground")
   .setFooter("© Professor Oak", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Larvitar-Pokemon-Go.png")
   
   .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 1024 characters.")
+  .addField("Available Larvitar Commands",
+    "To uncover even more information about Larvitar, use the commands below.")
  
-  .addField("!oak larvitar100", "100% CP search.", true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+  .addField("!oak larvitar100", "100% IV", true)
+  .addField("!oak larvitar98", "98%+ IV, 15 attack", true)
+  .addField("!oak larvitar96", "96%+ IV, 15 attack", true)
+  .addField("!oak larvitar93", "93%+ IV, 15 attack", true)
+  .addField("!oak larvitar91", "90%+ IV, 15 attack", true)
+  .addField("!oak larvitar0", "0% IV", true)
 
   message.channel.send({embed});
 }
