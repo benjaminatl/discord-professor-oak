47a0031f4cfb66ec60bce3aaf68b93286d2a37e3
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-14 17:43:39 -0400

    update

--- pokemon/kyogre.js
+++ pokemon/kyogre.js
@@ -10,14 +10,16 @@ module.exports.run = (client, message, args) => {
     .setImage("https://github.com/benjaminatl/discord-professor-oak/blob/master/images/kyogre-chart.jpg?raw=true")
     .setThumbnail("https://github.com/benjaminatl/discord-professor-oak/blob/master/images/kyogre.png?raw=true")
     
-    .setTimestamp()
-    .addField("Raid Boss Counters",
-      "Raukio, Venasaur, Zapdos, Exeggutor, Alola Exeggutor, Septile, Groudon, Rayquaza, Latios, Mewtwo, Salamance, Dragonite, Victreebel, Vileplume, Tangela")
-   
     .addField("Type", "Water & Electric", true)
     .addField("Counter Type", "Grass", true)
     .addField("Level 20 Max CP", "2328", true)
     .addField("Level 25 Max CP", "2910", true)
+
+    .setTimestamp()
+    .addField("Raid Boss Counters",
+      "Raukio, Venasaur, Zapdos, Exeggutor, Alola Exeggutor, Septile, Groudon, Rayquaza, Latios, Mewtwo, Salamance, Dragonite, Victreebel, Vileplume, Tangela")
+   
+    
   
     message.channel.send({embed});
   }
