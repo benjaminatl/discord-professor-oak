4ac022786e1c2269fedabba9742270b5d80c27d7
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-14 18:02:53 -0400

    update

--- pokemon/kyogre.js
+++ pokemon/kyogre.js
@@ -5,7 +5,7 @@ module.exports.run = (client, message, args) => {
     .setTitle("Kyogre - #")
     
     .setColor(0x00AE86)
-    .setDescription("`This Legendary Pokemon takes 5+ Trainers to defeat. Shiny form is available.")
+    .setDescription("This Legendary Pokemon takes 5+ Trainers to defeat. Shiny form is available.")
     .setFooter("© Professor Oak", "https://github.com/benjaminatl/discord-professor-oak/blob/master/images/professor-oak-default.jpg?raw=true")
     .setImage("https://github.com/benjaminatl/discord-professor-oak/blob/master/images/kyogre-chart.jpg?raw=true")
     .setThumbnail("https://github.com/benjaminatl/discord-professor-oak/blob/master/images/kyogre.png?raw=true")
@@ -14,8 +14,6 @@ module.exports.run = (client, message, args) => {
     .addField("Counter Type", "<:grassbadge:456939752591982623> <:electricbadge:456939752453570570>", true)
     .addField("Level 20 Max CP", "2328", true)
     .addField("Level 25 Max CP", "2910", true)
-
-    .addBlankField(true)
 
     .setTimestamp()
     .addField("Raid Boss Counters",
