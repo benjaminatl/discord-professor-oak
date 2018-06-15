d8694646f4b25d3ff65138f69adee5a3632c9f5b
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-15 13:36:26 -0400

    response update

--- directions/observatory.js
+++ directions/observatory.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Hello, Trainer. I see you need information on the gym **The Observatory At The University Of West Georgia**. My research shows: located on UWG campus, access is not hard, you may have to get out of the vehicle. https://goo.gl/maps/EApVEp1TENz").catch(console.error);
+    message.channel.send("Hello, Trainer. I see you need information on the gym **The Observatory At The University Of West Georgia**. My research shows: located on UWG campus, access is not hard, free parking lot located at the gym, you may or may not have to get out of the vehicle. https://goo.gl/maps/EApVEp1TENz").catch(console.error);
 }
