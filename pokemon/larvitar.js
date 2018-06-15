c1832f4de47f7a7967f9c3db9e1f13cf4d0f6a09
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-15 13:23:07 -0400

    update

--- pokemon/larvitar.js
+++ pokemon/larvitar.js
@@ -4,14 +4,13 @@ const embed = new Discord.RichEmbed()
 
   .setTitle("Larvitar - #246")
 
-  .addField("Egg", "10K", true)
   .addField("Shiny", "Yes", true)
   
   .setColor(0x00AE86)
   
   .setFooter("© Professor Oak", "https://github.com/benjaminatl/discord-professor-oak/blob/master/images/professor-oak-default.jpg?raw=true")
   .setThumbnail("https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Larvitar-Pokemon-Go.png")
-  
+  .setDescription("`Rock` and `Ground`")
   .setTimestamp()
   .addField("Extended Commands",
     "Uncover even more information about this Pokemon using the commands below. *A Pokemon followed by a number within a command, refers to IV's. These commands return a quick search that can be entered into the in-game Pokemon search bar.*")
