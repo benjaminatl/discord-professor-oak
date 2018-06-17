031478b8b13d14aed2eb5d7bf69f0dd4381b33ad
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-17 16:30:55 -0400

    update

--- pokemon/kyogre.js
+++ pokemon/kyogre.js
@@ -7,8 +7,8 @@ module.exports.run = (client, message, args) => {
     .setColor(0x00AE86)
     .setDescription("This Legendary Pokemon takes 5+ Trainers to defeat.")
     .setFooter("© Professor Oak", "https://github.com/benjaminatl/discord-professor-oak/blob/master/images/professor-oak-default.jpg?raw=true")
-    .setImage("https://github.com/benjaminatl/discord-professor-oak/blob/master/images/kyogre-chart.jpg?raw=true")
-    .setThumbnail("https://github.com/benjaminatl/discord-professor-oak/blob/master/images/3d-models/pokemon_icon_382_00.png?raw=true")
+    .setImage("https://github.com/benjaminatl/discord-professor-oak/blob/master/images//charts/kyogre-chart.jpg?raw=true")
+    .setThumbnail("https://github.com/benjaminatl/discord-professor-oak/blob/master/images/3dmodels/pokemon_icon_382_00.png?raw=true")
     
     .addField("Type", "<:waterbadge:434377739248730113> <:electricbadge:456939752453570570>", true)
     .addField("Counter Type", "<:grassbadge:456939752591982623> <:electricbadge:456939752453570570>", true)
