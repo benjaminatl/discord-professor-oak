0ac4a1bb7b93de823d123645053f900345b46b48
benjaminatl <33103968+benjaminatl@users.noreply.github.com>
2018-06-18 18:36:20 -0400

    new response

--- commands/alolan.js
+++ commands/alolan.js
@@ -1,59 +1,59 @@
 exports.run = (client, message, args) => {
-    message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
+  message.channel.send({embed: {
+  color: 3447003,
+  author: {
+    name: client.user.username,
+    icon_url: client.user.avatarURL
+  },
+  title: "Kanto Region, Alolan Forms",
+  description: "Hello Trainer! I see that you are interested in knowing more about the Alolan form Pokemon orignally discovered from the Kanto region. Based on my research, I have discovered 18 Pokemon along with their types. **NEW** Alolan Pokemon can be hatched from 7KM eggs that are can be obtained from receiving a special Gift from in-game Friends. You can use `!oak gifts` for more information.",
+  fields: [{
+      name: "Rattata, Raticate",
+      value: "Dark, Normal"
     },
-    title: "Kanto Region, Alolan Forms",
-    description: "Hello Trainer! I see that you are interested in knowing more about the Alolan form Pokemon orignally discovered from the Kanto region. Based on my research, I have discovered 18 Pokemon along with their types.",
-    fields: [{
-        name: "Rattata, Raticate",
-        value: "Dark, Normal"
-      },
-      {
-        name: "Raichu",
-        value: "Electric, Psychic"
-      },
-      {
-        name: "Sandshre, Sandslash",
-        value: "Ice, Steel"
-      },
-      {
-        name: "Vulpix, Ninetails",
-        value: "Ice, Fairy"
-      },
-      {
-        name: "Diglett, Dugtrio",
-        value: "Ground, Steel"
-      },
-      {
-        name: "Meowth, Persian",
-        value: "Dark"
-      },
-      {
-        name: "Geodude, Graveler, Golem",
-        value: "Rock, Electric"
-      },
-      {
-        name: "Grimer, Muk",
-        value: "Poison, Dark"
-      },
-      {
-        name: "Exeggutor",
-        value: "Grass, Dragon"
-      },
-      {
-        name: "Marowak",
-        value: "Fire, Ghost"
-      },
-             
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Professor Oak"
-    }
+    {
+      name: "Raichu",
+      value: "Electric, Psychic"
+    },
+    {
+      name: "Sandshre, Sandslash",
+      value: "Ice, Steel"
+    },
+    {
+      name: "Vulpix, Ninetails",
+      value: "Ice, Fairy"
+    },
+    {
+      name: "Diglett, Dugtrio",
+      value: "Ground, Steel"
+    },
+    {
+      name: "Meowth, Persian",
+      value: "Dark"
+    },
+    {
+      name: "Geodude, Graveler, Golem",
+      value: "Rock, Electric"
+    },
+    {
+      name: "Grimer, Muk",
+      value: "Poison, Dark"
+    },
+    {
+      name: "Exeggutor",
+      value: "Grass, Dragon"
+    },
+    {
+      name: "Marowak",
+      value: "Fire, Ghost"
+    },
+           
+  ],
+  timestamp: new Date(),
+  footer: {
+    icon_url: client.user.avatarURL,
+    text: "© Professor Oak"
   }
+}
 });
 }
