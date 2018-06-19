cbca542c0f76fa9b625bf47d9243741e42916d78
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:29:15 -0400

    Update striplingchapel.js

--- directions/striplingchapel.js
+++ directions/striplingchapel.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Hello, Trainer. I see you need information on the gym **Stripling Chapel**. My research shows: https://goo.gl/maps/1J5y2STrdYB2 http://maps.apple.com/?33.54101,-85.07365").catch(console.error);
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Stripling Chapel**. My research shows: https://goo.gl/maps/1J5y2STrdYB2").catch(console.error);
 }
