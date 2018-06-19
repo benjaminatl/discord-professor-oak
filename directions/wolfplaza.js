f5b87e59899ea70f32a38c08ca4cf0d380ec086d
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:41:58 -0400

    Create wolfplaza.js

---
+++ directions/wolfplaza.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Wolf Plaza**. My research shows: https://goo.gl/maps/Qf1zLAWHBQ32").catch(console.error);
+}
