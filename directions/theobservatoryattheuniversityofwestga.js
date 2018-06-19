99aefb5468734cca8abc6ed22b68f8610e76840e
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:31:53 -0400

    Create theobservatoryattheuniversityofwestga.js

---
+++ directions/theobservatoryattheuniversityofwestga.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **The Observatory At The University Of West Georgia**. My research shows: https://goo.gl/maps/4j19UZBj6pu").catch(console.error);
+}
