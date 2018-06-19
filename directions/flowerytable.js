901696d476fe9c84e1f3d161f34d2dd4edbe750b
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:33:59 -0400

    Create flowerytable.js

---
+++ directions/flowerytable.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **The Other Flowery Table**. My research shows: https://goo.gl/maps/2duGuh3uidM2").catch(console.error);
+}
