dbc86bc121d6f22e7005cb7b04978ca88db6529d
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:34:57 -0400

    Create trojanstadium.js

---
+++ directions/trojanstadium.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Trojan Stadium**. My research shows: https://goo.gl/maps/CDAFCCC3jDt").catch(console.error);
+}
