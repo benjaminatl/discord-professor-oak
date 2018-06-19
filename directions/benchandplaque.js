6269e343cb76c2aba085918801f54e5462ca6e56
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:44:17 -0400

    Create benchandplaque.js

---
+++ directions/benchandplaque.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **UWG Bench and Plaque**. My research shows: https://goo.gl/maps/Wb1yDtAYNK92").catch(console.error);
+}
