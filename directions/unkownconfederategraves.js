3918dde8b62e331e1c95b905ff231dbec4993aa2
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:51:51 -0400

    Create unkownconfederategraves.js

---
+++ directions/unkownconfederategraves.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Unknown Confederate Graves**. My research shows: https://goo.gl/maps/21sx75JTsFk").catch(console.error);
+}
