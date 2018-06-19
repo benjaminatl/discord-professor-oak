e45d385b0aebe8ed0916b96f488da2cb7b0bc8ae
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 13:50:18 -0400

    Create tanner.js

---
+++ directions/tanner.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Tanner Medical Center Med Library**. My research shows: https://goo.gl/maps/udWG78cw3VU2 http://maps.apple.com/?33.56931,-85.07648").catch(console.error);
+}
