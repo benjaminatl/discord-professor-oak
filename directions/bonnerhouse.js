aa062f191dc06a428b2b990a819509e6e241c81c
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:45:01 -0400

    Create bonnerhouse.js

---
+++ directions/bonnerhouse.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **UWG Bonner House**. My research shows: https://goo.gl/maps/uiAchVtznkQ2").catch(console.error);
+}
