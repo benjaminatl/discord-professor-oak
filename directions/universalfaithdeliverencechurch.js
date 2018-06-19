a0caee1ecc77f179c06d9c8d4ba76c576a2c78f6
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:36:04 -0400

    Create universalfaithdeliverencechurch.js

---
+++ directions/universalfaithdeliverencechurch.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Universal Faith Deliverence Church**. My research shows: https://goo.gl/maps/bPGtMFt2Vbo").catch(console.error);
+}
