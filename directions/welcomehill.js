343b0256d32596cce5933684f06890e4e16c87e0
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:39:15 -0400

    Create welcomehill.js

---
+++ directions/welcomehill.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Welcome Hill Baptist Church**. My research shows: This is also refered to as The Lofts, parking lot located at the church, does not require to exit the vehicle. https://goo.gl/maps/DDoYFQP756H2").catch(console.error);
+}
