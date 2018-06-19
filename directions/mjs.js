666e70c4a7c7f5d6765b9f81efb2c3256b5e5e57
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 14:03:20 -0400

    Update mjs.js

--- directions/mjs.js
+++ directions/mjs.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Hello, Trainer. I see you need information on the gym **St. Matthews At The Lake**. My research shows: This is also reffered to as MJ's, located at MJ's Buffet Resturant, free parking lots available, will need to exit the vehicle. https://goo.gl/maps/4x1bDPpLjFn http://maps.apple.com/?11=33.591521,-85.0466340").catch(console.error);
+    message.channel.send("Hello, Trainer. I see you need information on the gym **St. Matthews At The Lake**. My research shows: This is also reffered to as MJ's, located at MJ's Buffet Resturant, free parking lots available, will need to exit the vehicle. https://goo.gl/maps/4x1bDPpLjFn http://maps.apple.com/?11=-85.0466340,33.591521").catch(console.error);
 }
