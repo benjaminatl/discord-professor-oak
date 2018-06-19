efbd40dc7b82385254e998ad5f70b57f2942afac
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:38:20 -0400

    Create veteransmemorialpark.js

---
+++ directions/veteransmemorialpark.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Veterans Magnolia Memorial**. My research shows: https://goo.gl/maps/bPGtMFt2Vbo").catch(console.error);
+}
