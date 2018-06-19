fa33b25c028393081a070baf9acfa94d7843eab4
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:43:02 -0400

    Create turbowired.js

---
+++ directions/turbowired.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Turbo Wired**. My research shows: https://goo.gl/maps/6aCfRzHuPcr").catch(console.error);
+}
