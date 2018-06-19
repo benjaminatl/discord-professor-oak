eaeb7a9c522abf1f37c40198f7d2548921726aa3
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-06-19 15:46:16 -0400

    Create uwggazebo.js

---
+++ directions/uwggazebo.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **UWG Gazebo**. My research shows: https://goo.gl/maps/Ttipm7GHsZq").catch(console.error);
+}
