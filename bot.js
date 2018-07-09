3dd9815841d8d7ec0cf931413fd7d61e6e4d21ac
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-07-09 15:25:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,12 +27,6 @@ client.on("message", message => {
     console.error(err);
   }
   try {
-    let commandFile = require(`./searchstrings/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }
-  try {
     let commandFile = require(`./directions/${command}.js`);
     commandFile.run(client, message, args);
   } catch (err) {
